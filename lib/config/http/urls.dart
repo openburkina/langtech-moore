a34dbf6c27f087d23062d2b0aea6c9fbbbb056e7
nubes226 <nubes226>
2022-10-29 13:50:30 +0000

    Add server url

--- lib/config/http/urls.dart
+++ lib/config/http/urls.dart
@@ -1,5 +1,6 @@
 class Urls {
-  static const String API_BASE_URL = 'http://localhost:8080/api';
+  // static const String API_BASE_URL = 'http://178.128.248.138:8081/api';
+  static const String API_BASE_URL = 'http://localhost:8081/api';
   static const String LOGIN_URL = API_BASE_URL + '/authenticate';
   static const String REGISTER_URL = API_BASE_URL + '/register';
   static const String SOURCES_DATA_URL = API_BASE_URL + '/source-donnees';
